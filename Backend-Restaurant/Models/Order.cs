e682a9236db669433358cf7e3a805ba33572ce76
drm0011 <119403263+drm0011@users.noreply.github.com>
2025-03-20 10:50:48 +0100

    validation

--- Backend-Restaurant/Models/Order.cs
+++ Backend-Restaurant/Models/Order.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,9 @@ namespace Models
     public class Order
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Type is required.")]
         public string Type { get; set; } // food or drink
+        [Required(ErrorMessage = "Description is required.")]
         public string Description { get; set; }
         public bool IsCompleted { get; set; }
     }
