5783ddf281bc4e4e58f5e4a1bcb6e080cf0e1f06
Denzel Huijbers <huijbers15@outlook.com>
2025-05-08 11:59:40 +0200

    added Cors config

--- Backend-Restaurant/API-Access/Program.cs
+++ Backend-Restaurant/API-Access/Program.cs
@@ -3,27 +3,34 @@ using Dal;
 using Microsoft.EntityFrameworkCore;
 using Models;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 
 builder.Services.AddScoped<IOrderService, OrderService>();
 builder.Services.AddScoped<IOrderRepository, MockOrderRepository>();
 
 var app = builder.Build();
+
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
     //var context = services.GetRequiredService<RestaurandDbContext>();
 }
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -31,6 +38,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
