7bcf3f19e657cd634165767798dc1315bd9a331a
Rick S <290278@student.fontys.nl>
2025-06-19 10:17:43 +0200

    QR url for both fronends included

--- Backend-Restaurant/API-Access/Controllers/QrCodeController.cs
+++ Backend-Restaurant/API-Access/Controllers/QrCodeController.cs
@@ -19,7 +19,8 @@ namespace API_Access.Controllers
         [HttpGet("table/{tableId}")]
         public IActionResult GetTableQrCode(int tableId)
         {
-            string frontendUrl = "https://willemfrontend-fhfbeaewa5h0e3fw.germanywestcentral-01.azurewebsites.net";
+            //string frontendUrl = "https://willemfrontend-fhfbeaewa5h0e3fw.germanywestcentral-01.azurewebsites.net"; Link Ruben host
+            string frontendUrl = "https://willemfront-fbhjfrhydggqcfcy.westeurope-01.azurewebsites.net"; // Link Rick host
             string qrContent = $"{frontendUrl}/table/{tableId}";
 
             byte[] qrCodeImage = _qrCodeService.GenerateQrCode(qrContent);
